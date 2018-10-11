c80b6f08331a4e863920aa8e486f3cff32e86d97
Ionescu Alexandru <alx.ionescu97@gmail.com>
2018-10-11 14:19:17 +0300

    added gitignore

--- obj_dir/Vram__ALLsup.d
+++
@@ -1,4 +0,0 @@
-Vram__ALLsup.o: Vram__ALLsup.cpp Vram__Syms.cpp Vram__Syms.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h Vram.h
--- obj_dir/Vvga__ALLcls.d
+++
@@ -1,4 +0,0 @@
-Vvga__ALLcls.o: Vvga__ALLcls.cpp Vvga.cpp Vvga.h \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilatedos.h Vvga__Syms.h
