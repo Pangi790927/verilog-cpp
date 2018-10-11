fb26d73e9d6fd5e228cafffe7d536b6e372d1378
Pangi <andrpangr@gmail.com>
2018-10-11 19:02:35 +0300

    colors

--- vga.h
+++ vga.h
@@ -12,6 +12,25 @@ struct VGA {
 		CLEAR_BIT		= 0b0000'0000'0000'1000
 	};
 
+	enum {
+		Black			= 0,
+		Blue			= 1,
+		Green			= 2,
+		Cyan			= 3,
+		Red				= 4,
+		Magenta			= 5,
+		Brown			= 6,
+		Light_Gray		= 7,
+		Dark_Gray		= 8,
+		Light_Blue		= 9,
+		Light_Green		= 10,
+		Light_Cyan		= 11,
+		Light_Red		= 12,
+		Light_Magenta	= 13,
+		Yellow			= 14,
+		White			= 15
+	};
+
 	bool present = false;
 	std::shared_ptr<Vvga> chip = std::shared_ptr<Vvga>(new Vvga);
 	
@@ -89,6 +108,27 @@ struct VGA {
 	}
 
 	bool char_display[256][char_height][char_width];
+	
+	/* those colors are abgr not rgba reason is that */
+	uint colors[256] = {
+		0x0000'0000, // black
+		0x00ff'0000, // blue
+		0x0000'8000, // green
+		0x00ff'ff00, // cyan
+		0x0000'00ff, // red
+		0x00ff'00ff, // magenta
+		0x002A'2AA5, // brown 
+		0x00D3'D3D3, // light gray 
+
+		0x00A9'A9A9, // dark gray
+		0x00e6'D8Ad, // light blue
+		0x0000'ff00, // light green
+		0x00ff'ffe0, // light cyan
+		0x0000'0000, // light red
+		0x0000'0000, // light magenta
+		0x0000'0000, // yellow
+		0x0000'0000  // white
+	};
 };
 
 #endif
