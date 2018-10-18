24f8ab6eaf0133c2035f58237a07b62f80069df2
Ionescu Alexandru <alx.ionescu97@gmail.com>
2018-10-18 20:45:16 +0300

    Added some logi =c to the vga controller

--- control_pins.v
+++ control_pins.v
@@ -5,5 +5,6 @@
 `define VGA_WRITE	16'b0000_0000_0000_0001
 `define VGA_READ	16'b0000_0000_0000_0010
 
-`define VGA_WRITE_PIN	0
-`define VGA_READ_PIN	1
+`define VGA_WRITE_PIN		0
+`define VGA_READ_PIN		1
+`define VGA_HW_WRITE_PIN 	2
