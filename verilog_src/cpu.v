5ac38c30b53baa680a73c6e69c8c9f68f452d8e5
iAlex97 <alx.ionescu97@gmail.com>
2019-03-28 10:14:00 +0200

    wip

--- verilog_src/cpu.v
+++ verilog_src/cpu.v
@@ -1,4 +1,5 @@
 `include "verilog_src/alu.v"
+`include "verilog_src/cpu_states.v"
 
 module cpu(
 		input	clk,
@@ -11,6 +12,9 @@ module cpu(
 	);
 
 	parameter word_width = 32;
+
+	reg[word_width-1 : 0] 		state 		= 0;
+	reg[word_width-1 : 0] 		next_state	= 0;
 
 	// instantiate T1 register and related connections
 	wire                        t1_oe;
@@ -33,4 +37,53 @@ module cpu(
 	wire[word_width-1 : 0]      alu_out;
 	wire[4 : 0]                 alu_flags;
 	alu #(word_width, 5) alu(alu_oe, alu_opcode, t1_out, t2_out, alu_carry, alu_out, alu_flags);
+
+	// FSM - sequential part
+	always @(posedge clk) begin
+		state <= `C_STATE_RESET;
+
+		if(!rst)
+			state <= next_state;
+	end
+
+	// FSM - combinational part
+	always @(state) begin
+		t1_we = 0;
+		t1_oe = 0;
+
+		t2_we = 0;
+		t2_oe = 0;
+
+		case(state)
+			`C_STATE_RESET: begin
+				next_state = `C_STATE_TEST;
+			end
+
+			`C_STATE_TEST: begin
+				t1_in = 5;
+				t1_we = 1;
+
+				t2_in = 6;
+				t2_we = 1;
+
+				next_state = `C_STATE_TEST + 1;
+			end
+
+			`C_STATE_TEST + 1: begin
+				t1_oe = 1;
+				t2_oe = 1;
+
+				res = t1_out;
+				//$display("t2 content: %d\n", t2_out);
+			
+				next_state = `C_STATE_TEST + 2;
+			end
+
+			`C_STATE_TEST + 2: begin
+				$display("t1 content: %d\n", res);
+
+				next_state = `C_STATE_TEST + 3;
+			end
+		endcase
+	end
 endmodule
---
+++ verilog_src/cpu_states.v
@@ -0,0 +1,2 @@
+`define C_STATE_RESET			'h00 // reset state
+`define C_STATE_TEST			'h10 // load instruction to instruction register
