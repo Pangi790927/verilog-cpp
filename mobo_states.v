36fb85cd229a7a8d9f0d68f15866595a0a2247ea
Pangi <andrpangr@gmail.com>
2019-03-28 12:07:43 +0200

    dbg

--- mobo_states.v
+++
@@ -1,8 +0,0 @@
-`define M_STATE_WRITE			0
-`define M_STATE_WAIT_WRITE		1
-
-`define M_STATE_READ			2
-`define M_STATE_WAIT_READ		3
-
-`define M_STATE_VGA_WRITE		4
-`define M_STATE_VGA_WAIT		5
--- verilog_src/cpu_states.v
+++ verilog_src/cpu_states.v
@@ -3,7 +3,6 @@
 `define C_STATE_HLT				'h20 // load instruction to instruction register
 
 function string cpu_str_state(int state);
-	$display("state: %x", state & ~'hf);
 	case (state & ~'hf)
 		`C_STATE_RESET:	return "C_STATE_RESET";
 		`C_STATE_TEST:	return "C_STATE_TEST";
