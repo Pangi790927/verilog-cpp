d4bd652ba97cced83965ce259607109078aaa133
Pangi <andrpangr@gmail.com>
2019-09-17 20:44:35 +0300

    phase0-1 interface

--- dcasm/asm_instr.h
+++ dcasm/asm_instr.h
@@ -5,13 +5,9 @@
 #include "str_helper.h"
 #include "debug.h"
 
-struct Instr {
-	int line_cnt;
-
-	Instr(int line_cnt) : line_cnt(line_cnt) {}
-	virtual void polymorfic() {}
-};
-std::map<std::string, AsmInstr&> map;
+// label:
+	// .local
+// label.local
 // (line, dir, label, line, word_cnt, addr)
 struct AsmInstr {
 	bool is_local;
@@ -22,29 +18,6 @@ struct AsmInstr {
 	std::string line;
 	int word_cnt;
 	int addr;
-};
-
-struct AsmInstr : Instr {
-	std::string instr;
-	std::string args;
-	int wordCount;
-	int free_reg = -1;
-
-	AsmInstr(int line_cnt, std::string instr, std::string args)
-	: Instr(line_cnt), instr(instr), args(args), wordCount(1) {}
-};
-
-struct LabelInstr : Instr {
-	std::string label;
-	bool isLocal = false;
-	std::string parrent;
-
-	LabelInstr(int line_cnt, std::string label, std::string parrent = "")
-	: label(label), parrent(parrent), Instr(line_cnt)
-	{
-		if (parrent != "")
-			isLocal = true;
-	}
 };
 
 #endif
